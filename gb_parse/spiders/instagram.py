44b803739f6f12d7fae635d3d539f752052e103d
gefest <gefest@me.com>
2021-02-06 20:05:41 +0300

    les6 HW

--- gb_parse/spiders/instagram.py
+++ gb_parse/spiders/instagram.py
@@ -1,9 +1,7 @@
+import datetime as dt
 import json
 import scrapy
-
-# TODO Авторизоваться
-# TODO Перейти на странички тегов
-# TODO Собрать информацию включая пагинацию
+from ..items import InstaTag, InstaPost, InstaUser, InstaFollow
 
 
 class InstagramSpider(scrapy.Spider):
@@ -11,37 +9,74 @@ class InstagramSpider(scrapy.Spider):
     allowed_domains = ["www.instagram.com"]
     start_urls = ["https://www.instagram.com/"]
     login_url = "https://www.instagram.com/accounts/login/ajax/"
+    api_url = "/graphql/query/"
+    query_hash = {
+        "posts": "56a7068fea504063273cc2120ffd54f3",
+        "tag_posts": "9b498c08113f1e09617a1703c22b2f32",
+    }
 
     def __init__(self, login, password, *args, **kwargs):
         self.tags = ["python", "программирование", "developers"]
         self.login = login
-        self.password = password
+        self.enc_passwd = password
         super().__init__(*args, **kwargs)
 
-    def parse(self, response):
+    def parse(self, response, **kwargs):
         try:
-            js_data = self.js_data_extractor(response)
+            js_data = self.js_data_extract(response)
             yield scrapy.FormRequest(
                 self.login_url,
                 method="POST",
                 callback=self.parse,
                 formdata={
                     "username": self.login,
-                    "enc_password": self.password,
+                    "enc_password": self.enc_passwd,
                 },
-                headers={
-                    "X-CSRFToken": js_data["config"]["csrf_token"],
-                },
+                headers={"X-CSRFToken": js_data["config"]["csrf_token"]},
             )
-        except AttributeError:
-            print(1)
-            if response.json().get('authenticated'):
+        except AttributeError as e:
+            if response.json().get("authenticated"):
                 for tag in self.tags:
-                    yield response.follow(f'/explore/tags/{tag}/', callback=self.tag_parse)
-    
-    def tag_parse(self, response):
-        print(1)
+                    yield response.follow(f"/explore/tags/{tag}/", callback=self.tag_parse)
 
-    def js_data_extractor(self, response) -> dict:
-        script = response.xpath('//body/script[contains(text(), "csrf_token")]/text()').get()
-        return json.loads(script.replace("window._sharedData = ", "", 1)[:-1])
+
+    def tag_parse(self, response):
+        tag = self.js_data_extract(response)["entry_data"]["TagPage"][0]["graphql"]["hashtag"]
+
+        yield InstaTag(
+            date_parse=dt.datetime.utcnow(),
+            data={
+                "id": tag["id"],
+                "name": tag["name"],
+                "profile_pic_url": tag["profile_pic_url"],
+            },
+        )
+        yield from self.get_tag_posts(tag, response)
+
+    def tag_api_parse(self, response):
+        yield from self.get_tag_posts(response.json()["data"]["hashtag"], response)
+
+    def get_tag_posts(self, tag, response):
+        if tag["edge_hashtag_to_media"]["page_info"]["has_next_page"]:
+            variables = {
+                "tag_name": tag["name"],
+                "first": 100,
+                "after": tag["edge_hashtag_to_media"]["page_info"]["end_cursor"],
+            }
+            url = f'{self.api_url}?query_hash={self.query_hash["tag_posts"]}&variables={json.dumps(variables)}'
+            yield response.follow(
+                url,
+                callback=self.tag_api_parse,
+            )
+
+        yield from self.get_post_item(tag["edge_hashtag_to_media"]["edges"])
+
+    @staticmethod
+    def get_post_item(edges):
+        for node in edges:
+            yield InstaPost(date_parse=dt.datetime.utcnow(), data=node["node"])
+
+    @staticmethod
+    def js_data_extract(response):
+        script = response.xpath('//script[contains(text(), "window._sharedData =")]/text()').get()
+        return json.loads(script.replace("window._sharedData =", "")[:-1])

8f88d498d07e7c5985102c088de8584a3146f881
gefest <gefest@me.com>
2021-02-10 20:15:13 +0300

    les7 HW

--- gb_parse/spiders/instagram.py
+++ gb_parse/spiders/instagram.py
@@ -13,10 +13,15 @@ class InstagramSpider(scrapy.Spider):
     query_hash = {
         "posts": "56a7068fea504063273cc2120ffd54f3",
         "tag_posts": "9b498c08113f1e09617a1703c22b2f32",
+        "follow": "d04b0a864b4b54837c0d870b0e77e076",
+        "followers": "c76146de99bb02f6415203be841dd25a",
     }
 
     def __init__(self, login, password, *args, **kwargs):
         self.tags = ["python", "программирование", "developers"]
+        self.users = [
+            "teslamotors",
+        ]
         self.login = login
         self.enc_passwd = password
         super().__init__(*args, **kwargs)
@@ -36,9 +41,52 @@ class InstagramSpider(scrapy.Spider):
             )
         except AttributeError as e:
             if response.json().get("authenticated"):
-                for tag in self.tags:
-                    yield response.follow(f"/explore/tags/{tag}/", callback=self.tag_parse)
+                # for tag in self.tags:
+                #     yield response.follow(f"/explore/tags/{tag}/", callback=self.tag_parse)
+                for user in self.users:
+                    yield response.follow(f"/{user}/", callback=self.user_page_parse)
 
+    def user_page_parse(self, response):
+        user_data = self.js_data_extract(response)["entry_data"]["ProfilePage"][0]["graphql"]["user"]
+        yield InstaUser(date_parse=dt.datetime.utcnow(), data=user_data)
+
+        yield from self.get_api_follow_request(response, user_data)
+
+    def get_api_follow_request(self, response, user_data, variables=None):
+        if not variables:
+            variables = {
+                "id": user_data["id"],
+                "first": 100,
+            }
+        url = f'{self.api_url}?query_hash={self.query_hash["follow"]}&variables={json.dumps(variables)}'
+        yield response.follow(
+            url, callback=self.get_api_follow, cb_kwargs={"user_data": user_data}
+        )
+
+    def get_api_follow(self, response, user_data):
+        if b"application/json" in response.headers["Content-Type"]:
+            data = response.json()
+            yield from self.get_follow_item(
+                user_data, data["data"]["user"]["edge_follow"]["edges"]
+            )
+            if data["data"]["user"]["edge_follow"]["page_info"]["has_next_page"]:
+                variables = {
+                    "id": user_data["id"],
+                    "first": 100,
+                    "after": data["data"]["user"]["edge_follow"]["page_info"]["end_cursor"],
+                }
+                yield from self.get_api_follow_request(response, user_data, variables)
+
+    def get_follow_item(self, user_data, follow_users_data):
+        for user in follow_users_data:
+            yield InstaFollow(
+                user_id=user_data["id"], # этот пользователь
+                user_name=user_data["username"],
+                follow_id=user["node"]["id"], #на этого пользователя
+                follow_name=user["node"]["username"],
+            )
+
+            yield InstaUser(date_parse=dt.datetime.utcnow(), data=user["node"])
 
     def tag_parse(self, response):
         tag = self.js_data_extract(response)["entry_data"]["TagPage"][0]["graphql"]["hashtag"]
